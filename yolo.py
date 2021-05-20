e049b6f165318bfa28fa6622da9a58385d8dbb43
gokulesh-danapal <dnplgokulesh@gmail.com>
2021-05-20 11:09:42 +0200

    yolo.py modified

--- yolo.py
+++ yolo.py
@@ -15,7 +15,7 @@ hyp = { 'device':'cuda', #Intialise device as cpu. Later check if cuda is avaial
         'lr0': 0.01,  # initial learning rate (SGD=1E-2, Adam=1E-3)
         'momentum': 0.937,  # SGD momentum/Adam beta1
         'weight_decay': 0.0005,  # optimizer weight decay
-        'anchors_g': [[12, 16], [19, 36], [40, 28], [36, 75], [76, 55], [72, 146], [142, 110], [192, 243], [459, 401]],
+        #'anchors_g': [[12, 16], [19, 36], [40, 28], [36, 75], [76, 55], [72, 146], [142, 110], [192, 243], [459, 401]],
         'nclasses': 4, #Number of classes
         'names' : ['person', 'RidableVehicle', 'car', 'LargeVehicle'],
         'gs': 32, #Image size multiples
@@ -29,6 +29,7 @@ hyp = { 'device':'cuda', #Intialise device as cpu. Later check if cuda is avaial
         'multi_scale': False, #Bool to do multi-scale training
         'test_all': True, #Run test after end of each epoch
         'save_all': True, #Save checkpoints after every epoch
+        'auto_anchor': False, #create new anchors by Kmeans clustering
         
         'giou': 0.05,  # GIoU loss gain
         'cls': 0.025,  # cls loss gain
@@ -52,6 +53,16 @@ hyp = { 'device':'cuda', #Intialise device as cpu. Later check if cuda is avaial
         'flipud': 0.0,  # image flip up-down (probability)
         'fliplr': 0.5,  # image flip left-right (probability)
         'mixup': 0.0, #mix up probability
+        
+        'anchors_g': [[9.72318281, 89.90291182],
+                    [27.38753188, 76.11763602],
+                    [ 17.16083467, 163.93638637],
+                    [ 48.64984549, 127.39453151],
+                    [ 32.2540104 , 286.52858829],
+                    [ 86.4122153 , 222.36901603],
+                    [100.69202021, 821.01962072],
+                    [155.38462465, 351.55357456],
+                    [253.37522873, 768.02296055]]
      }
 
 
